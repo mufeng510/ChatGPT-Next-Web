aa42aa2fe7f232c7ce4ea0f04df3ea3fcf045217
jijiangrui <379369845@qq.com>
2023-09-15 10:28:50 +0800

    feat: modify title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT Next",
+  description: "强大且丝滑的ChatGPT",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatGPT Next",
     statusBarStyle: "default",
   },
 };
